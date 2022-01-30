a8aa3151b1e33531fa08146845736c8e32027711
Akash <93872368+AkashH-20@users.noreply.github.com>
2022-01-30 16:08:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function compute() {
+  var principal = parseFloat(document.getElementById("principal").value);
+  var rate = parseFloat(document.getElementById("rate").value);
+  var years = parseInt(document.getElementById("years").value);
+  var interest = principal * years * rate / 100;
+  var yearInTheFuture = new Date().getFullYear() + years;
+  //Create the Interest text
+  document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp at an interest rate of <mark>" 
+  + rate + "%" + "</mark>,<br/>" + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp You will receive an amount of <mark>" + interest + "</mark>,<br/> "
+  + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp in the year <mark>" + yearInTheFuture + "</mark>";
+
+}
+
+function getSliderValue() {
+  document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+function validateAmount() {
+  var principal = document.getElementById("principal").value;
+  var biggerThanZero = parseInt(principal) > 0;
+  if (!biggerThanZero) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  }
+
+}
